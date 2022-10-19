a9ac25e433d7d75fbab706dd410572b006b786f5
kihiuFrank <franklinekihiu@gmail.com>
2022-10-19 13:58:45 +0300

    forking mainnet

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,13 +1,12 @@
 /* eslint-disable no-process-exit */
 // yarn hardhat node
-// yarn hardhat run scripts/readPrice.js --network localhost
+// yarn hardhat run scripts/aaveBorrow.js --network localhost
 const { ethers } = require("hardhat")
+const { getWeth } = require("./getWeth")
 
 async function main() {
     //protocol treats everything as ERC20 token
-    const priceConsumerV3 = await ethers.getContract("PriceConsumerV3")
-    const price = await priceConsumerV3.getLatestPrice()
-    console.log(price.toString())
+    await getWeth()
 }
 
 main()
--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -6,7 +6,7 @@ async function getWeth() {
     // we need abi , contract address
     // 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
     const iWeth = await ethers.getContractAt(
-        "Iweth",
+        "IWeth",
         "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
         deployer
     )
