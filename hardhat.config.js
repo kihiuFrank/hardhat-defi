d39be4c09ae93fec9915140eb0babb0a9ec5d44f
kihiuFrank <franklinekihiu@gmail.com>
2022-10-20 14:08:53 +0300

    depositing into Aave

--- hardhat.config.js
+++ hardhat.config.js
@@ -46,6 +46,9 @@ module.exports = {
                 version: "0.6.12",
             },
             {
+                version: "0.6.6",
+            },
+            {
                 version: "0.4.19",
             },
         ],
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,12 +1,48 @@
 /* eslint-disable no-process-exit */
 // yarn hardhat node
 // yarn hardhat run scripts/aaveBorrow.js --network localhost
-const { ethers } = require("hardhat")
-const { getWeth } = require("./getWeth")
+const { ethers, getNamedAccounts } = require("hardhat")
+const { getWeth, AMOUNT } = require("./getWeth")
 
 async function main() {
     //protocol treats everything as ERC20 token
     await getWeth()
+    const { deployer } = await getNamedAccounts()
+
+    // to interact with the aave protocol we need.
+    // abi
+    // contract address
+    // lending pool address provider: 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+    const lendingPool = await getLendingPool(deployer)
+    console.log(`LendingPool address ${lendingPool.address}`)
+
+    //deposit!
+    const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+    // approve
+    await approveErc20(wethTokenAddress, lendingPool.address, AMOUNT, deployer)
+    console.log("Depositing...")
+    //the deposit function requires the following params (https://docs.aave.com/developers/v/2.0/the-core-protocol/lendingpool)
+    await lendingPool.deposit(wethTokenAddress, AMOUNT, deployer, 0)
+    console.log("Deposited!")
+}
+
+async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("Approved!")
+}
+
+async function getLendingPool(account) {
+    const lendingPoolAddressesProvider = await ethers.getContractAt(
+        "ILendingPoolAddressesProvider",
+        "0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5",
+        account
+    )
+    const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
+
+    return lendingPool
 }
 
 main()
